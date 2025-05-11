d8bae84daae7c08e091e11e3994765a2f45a355f
Parker Freestone <parkerfreestone@gmail.com>
2025-05-11 01:46:59 -0600

    :bug: Guess we have to load them here too?

--- src/cli/sync.ts
+++ src/cli/sync.ts
@@ -1,7 +1,9 @@
 import { generateTypes } from "./generate-types";
 import { sync as syncSchema } from "../db";
+import { loadModels } from "../orm";
 
 export const sync = async () => {
+  await loadModels();
   await syncSchema();
   await generateTypes();
 };
